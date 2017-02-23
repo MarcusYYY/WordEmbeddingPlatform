27c676ae5bdf0b867da56f193cf9792824122dc6
Marcus Yu <marcusyuzc1992@gmail.com>
2017-02-23 16:38:13 -0600

    clean the code for close_word_comparison.

--- word_token.py
+++ word_token.py
@@ -4,25 +4,14 @@ import numpy as np
 import keras
 np.random.seed(1360)
 from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils.np_utils import to_categorical
-from keras.layers import Dense, Input, Flatten
-from keras.layers import Conv1D, MaxPooling1D, Embedding
-from keras.models import Model
 import sys
 from scipy import spatial
+
 
 BASE_DIR = ''
 GLOVE_DIR = BASE_DIR + 'glove.6B/'
 TEXT_DATA_DIR = BASE_DIR + '20_newsgroup'
-MAX_SEQUENCE_LENGTH = 1000
 MAX_NB_WORDS = 2000
-EMBEDDING_DIM = 100 
-VALIDATION_SPLIT = 0.2
-# first, build index mapping words in the embeddings set
-# to their embedding vector
-
-# print('Indexing word vectors.')
 
 #compute the overlap close words of same word in different embeddings 
 def Score_similarity(ref_1,ref_2):
@@ -49,11 +38,6 @@ for line in f:
         continue
 f.close()
 
-# print('Found %s word vectors.' % len(embeddings_index))
-
-# second, prepare text samples and their labels_index
-# print('Processing text dataset')
-
 texts = []  # list of text samples
 labels_index = {}  # dictionary mapping label name to numeric id
 labels = []  # list of label ids
@@ -73,22 +57,12 @@ for name in sorted(os.listdir(TEXT_DATA_DIR)):
                 f.close()
                 labels.append(label_id)
 
-# print('Found %s texts.' % len(texts))
-
 # finally, vectorize the text samples into a 2D integer tensor
 tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
 word_index = tokenizer.word_index
 
-# print('Found %s unique tokens.' % len(word_index))
-
-data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-labels = to_categorical(np.asarray(labels))
-# print('Shape of data tensor:', data.shape)
-# print('Shape of label tensor:', labels.shape)
-
-# pick up the reference word in train corpus
 word_rank = sorted(word_index,key = word_index.__getitem__)
 indices = np.arange(len(word_index)/100)
 np.random.shuffle(indices)
@@ -149,31 +123,4 @@ for path in os.listdir(GLOVE_DIR + '25MB'):
     final_ans.append(path)
     final_ans.append(Score_similarity(close_word,close_word_))
 print(final_ans)
-# for path in os.listdir(GLOVE_DIR + '25MB'):
-#     embeddings_index_ = {}
-#     f = open(os.path.join(GLOVE_DIR + '25MB/', path))
-#     print (path)
-#     count = 0
-#     for line in f:
-#         if count == 0:
-#             count = count + 1
-#             continue
-#         values = line.split()
-#         word = values[0]
-#         try:
-#             coefs = np.asarray(values[1:], dtype='float32')
-#             embeddings_index_[word] = coefs
-#         except:
-#             continue
-#     f.close()
-#     distance = 0
-#     close_word_ = {}
-#     for word in reference:
-#         dist = {}
-#         for key, val in embeddings_index_.iteritems():
-#             dist[key] = 1 - spatial.distance.cosine(val,reference_matrix[word])
-#             distance = distance + dist[key]
-#         dist = sorted(dist,key = dist.__getitem__,reverse = True)
-#         ans = [key for key in dist]
-#         close_word_[word] = ans[0:20]
-#     print(close_word_)
+
