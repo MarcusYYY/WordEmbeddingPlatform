75897e949df1557a0ba5eb4cf8c2d3fec6664231
Marcus Yu <marcusyuzc1992@gmail.com>
2017-03-01 20:51:56 -0600

    added reference module and rewrited download module.

--- NU_embedding.py
+++ NU_embedding.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import urllib,urllib2,requests,numpy,sys,os,zipfile
+from scipy import spatial
 
 def report(count, blockSize, totalSize):
 	percent = float(count*blockSize*100/totalSize)
@@ -41,12 +42,14 @@ class embedding:
 			print 'The embedding you are looking for does not exist'
 			self.flag = False
 		self.url = url
-		self.size = embedding.embedding_list[embedding.embedding_names == name]['vocabulary size'].values[0]
+		self.size = int(embedding.embedding_list[embedding.embedding_names == name]['vocabulary size'].values[0])
 		self.path = path
 		self.dl = False
 		self.destination = None
-
-	# download the embeddings in the broker file on data.world
+		self.vector = None
+		self.embed = None
+	# download the embeddings in the broker file on data.world and save them 
+	# on the local files.
 
 	def download(self):
 		if self.flag:
@@ -56,19 +59,20 @@ class embedding:
 			if form == 'zip':
 				name = url.split('/')[-1]
 				self.destination = path + name
-				print self.destination
 				urllib.urlretrieve(url,path + name,reporthook = report)
+				print 'The embedding directory is %s .' % self.destination
 			else:
 				file = pd.read_table(url)
 				file.to_csv(path + self.name + '.txt',header = None, index = None ,mode = 'a',)
-				self.destination = path + name + '.txt'
+				self.destination = path + self.name + '.txt'
+				print 'The embedding directory is %s .' % self.destination
 			self.dl = True
 		else:
 			print "You can't download the embedding because errors happened."
-
-	# pick any number of reference words and find any number of close words in the current embedding
-	def Reference_word(self,num_refer,num_closeWord,num_sample = 10):
+		
 		embed = None
+
+		#extract embedding from zip file or txt file.
 		if self.dl:
 			if zipfile.is_zipfile(self.destination):
 				zf = zipfile.ZipFile(self.destination,'r')
@@ -85,8 +89,58 @@ class embedding:
 							print 'ERROR: Did not find %s in zip file' % filename
 				else:
 					embed = zf.read(names[0])
-		return embed
+			else:
+				file = open(self.destination)
+				embed = file.read()
+			self.embed = embed
+			#store the word vector into dictionary
+			word_vector = {}
+			cach = embed.split('\n')
+			for i,row in enumerate(cach):
+				if i == 0:
+					continue
+				values = row.split()
+				try:
+					word = values[0]
+					coefs = numpy.asarray(values[1:],dtype = 'float32')
+					word_vector[word] = coefs
+				except:
+					continue
+			self.vector = word_vector
+			print 'Word embedding has been successfully downloaded.'
+			return word_vector
+		else:
+			print "The embedding you asked for has not been successfully downloaded. "
+	# pick up any number of reference words and find any number of close words in the current embedding
 	
-A = embedding('glove.6B',100,'2016Spring/')
-A.download()
-data = A.Reference_word(100,100,10)
+	def CloseWord_test(self,num_refer = 10,num_closeWord = 10,iter_times = 10,seed = numpy.random.randint(1000)):
+		numpy.random.seed(seed)
+		if self.dl:
+			refer_idx = numpy.random.randint(self.size,size = num_refer,dtype = numpy.int64)
+			vocab = [self.embed.split('\n')[idx].split()[0] for idx in refer_idx]
+			refer_matrix = {}
+			for word in vocab:
+				try:
+					refer_matrix[word] = embed[word]
+				except:
+					continue
+			#find the most close num_closeWord words for every reference word
+			close_word = {}
+			for word in vocab:
+				dist = {}
+				for key,val in embed.iteritems():
+					try:
+						dist[key] = 1 - spatial.distance.cosine(val,refer_matrix[word])
+					except:
+						continue
+				dist = sorted(dist,key = dist.__getitem__,reverse = True)
+				ans = [key for key in dist]
+				close_word[word] = ans[1:num_closeWord]
+			return close_word,seed
+		else:
+			print 'The embedding has not been downloaded yet.'
+A = embedding('NYT_Art',100,'2016Spring/')
+embed = A.download()
+data,seed = A.CloseWord_test(10,10,10)
+print data,seed
+
