b0653d0c3dd5d835ce03ccd5fe208f5c4f950f25
Marcus Yu <marcusyuzc1992@gmail.com>
2017-04-06 17:46:20 -0500

    All tests passed and this is the illustration of how to use this platform.

--- Embedding_platform/test.py
+++ Embedding_platform/test.py
@@ -1,11 +1,24 @@
-import Northwestern_embedding as Nu
+import Embedding_platform as ep
 
-# Nu.check()
-# Nu.query_embeddings('arts_40','you')
-# Nu.EmbedExtract(file_dir ='/Users/Marcus/Desktop/reuters/reutersR8_all' ,table ='ArtDanceMusic',pad = True)
-a = Nu.embedding('agriculture_40',100)
-a.download()
-# print a.vector
-# Nu.HighDensityVocab()
-# Nu.RankVocabGenerator()
-#Nu.method_a('/Users/Marcus/Desktop/reuters/reutersR8_all',5000,5000,200)
+#Check the information of all avaiable embeddings existing in the broker.csv
+ep.check()
+
+# Query for a specified word in specfied embedding.
+ep.query_embeddings('arts_40','you')
+ep.query_embeddings('ArtDanceMusic','the')
+
+# Extracted necessary parts of pre_trained embeddings for given corpus input. 
+# 'pad' = True by default in order to add zeros to fill up our embedding for missing words.
+# 'check' = True by default in order to display embeddings we extacted.
+ep.EmbedExtract(file_dir ='/Users/Marcus/Desktop/reuters/reutersR8_all' ,table ='art_40',pad = True,check = True)
+ep.EmbedExtract(file_dir ='/Users/Marcus/Desktop/reuters/reutersR8_all' ,table ='arts_40',pad = True,check = True)
+
+# Initiate word embedding by specifying its name and dimension.
+# Initiation will fail if wrong dimension parameter given or embedding doesn't exist.
+a = ep.embedding('agriculture_40',100)
+a.download(path = '2016spring/')
+print a.vector
+
+INPUT_DIR = '/Users/Marcus/Desktop/reuters/reutersR8_all'
+# Embedding selection by method A.
+ep.method_a(inp_dir = INPUT_DIR,num_sig = 5000,num_sig_embedding = 5000,num_stopwords = 200)
