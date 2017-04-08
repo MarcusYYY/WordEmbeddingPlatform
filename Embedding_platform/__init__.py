b87666291ed63b3e50eb16d6e498fa7abfbc910d
Marcus Yu <marcusyuzc1992@gmail.com>
2017-04-07 21:39:06 -0500

    added files.

---
+++ Embedding_platform/__init__.py
@@ -0,0 +1,16 @@
+"""
+WordEmbeddingPlatform
+Copyright 2017 Northwestern University
+
+This is a word embedding selection , query and download platform of which function is to
+facilitate natural language processing researcher to use word embeddings more efficiently.
+
+Associated with this platform is a broker-centered model. You might want to check the details
+at http://40i123952-4-24305=234052o=2.com
+
+"""
+
+from __future__ import absolute_import
+
+__version__ = '1.0.0-beta.1'
+
--- Embedding_platform/_init_.py
+++ Embedding_platform/_init_.py
@@ -1,17 +1,7 @@
 """
-WordEmbeddingPlatform
-Copyright 2017 Northwestern University
-
-This is a word embedding selection , query and download platform of which function is to
-facilitate natural language processing researcher to use word embeddings more efficiently.
-
-Associated with this platform is a broker-centered model. You might want to check the details
-at http://40i123952-4-24305=234052o=2.com
-
+This file saves all broker.csv and pre_trained word embedding url.
 """
 
-from __future__ import absolute_import
 
-__version__ = '1.0.0-beta.1'
 broker_url = 'https://query.data.world/s/4o7h67xz3m3zgsui7tk08yst8'
 table_parser = "marcusyyy/northwestern-word-embedding"
